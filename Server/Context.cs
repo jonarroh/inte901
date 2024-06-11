2c3771a39f52b6dfc9605db177aceb6fada6c9b1
jonarroh <jonarrodi99@gmail.com>
2024-06-10 23:44:55 -0600

    first commit

---
+++ Server/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.models;
+
+namespace Server
+{
+    public class Context(DbContextOptions<Context> options) : DbContext(options)
+    {
+        public DbSet<Test> Tests { get; set; }
+
+    }
+}
---
+++ Server/models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
