f8186b52d6fc2ab36403167725397cfa39e16504
jonarroh <jonarrodi99@gmail.com>
2024-06-23 16:45:55 -0600

    Commit before update

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 
 @Component({
   selector: 'LoginComponent',
   standalone: true,
-  imports: [],
+  imports: [HlmInputDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,11 +5,11 @@ module.exports = {
     extend: {
       colors: {
         primary: "#F5F5F5",
-        secondary: "#da8efa",
+        secondary: "#A6754C",
         accent: "#7768bb",
         neutral: "#B1D4FF",
         "base-100": "#B1D4FF",
-        background: "#66CCCC",
+        background: "#8CC896",
         info: "#3399FF",
         success: "#F78DB1",
         warning: "#515d8c",
