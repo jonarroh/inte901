3d4a5c77e6b5634e44dbb203e45660de33b6ab62
jonarroh <jonarrodi99@gmail.com>
2024-06-23 10:46:30 -0600

    Init login page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#3490dc",
-        secondary: "#ffed4a",
-        destructive: "#e3342f",
+        primary: "#F5F5F5",
+        secondary: "#da8efa",
+        accent: "#7768bb",
+        neutral: "#B1D4FF",
+        "base-100": "#B1D4FF",
+        background: "#66CCCC",
+        info: "#3399FF",
+        success: "#F78DB1",
+        warning: "#515d8c",
+        error: "#E14E64",
       },
     },
   },
