172a3a54fbfae8e74d395a5f39e263dc899986ba
jonarroh <jonarrodi99@gmail.com>
2024-06-25 09:02:28 -0600

    fix: replace api message in login

--- Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288b8c418029790fb7b96f5193e5a329b9ef21f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05596ec848fd7b4f6967ac6de0376169d069f293")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
