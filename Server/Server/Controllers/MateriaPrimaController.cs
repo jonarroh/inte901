198cb4497b2f4e65c16cfbac42753120dd26151c
Daniel Valderrama <danimanri05@gmail.com>
2024-06-25 23:39:22 -0600

    Desarrollo de controlador de materia prima

---
+++ Server/Server/Controllers/MateriaPrimaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MateriaPrimaController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public MateriaPrimaController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MateriaPrima
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MateriaPrima>>> GetMateriaPrimas()
+        {
+            return await _context.MateriasPrimas.ToListAsync();
+        }
+
+        // GET: api/MateriaPrima/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MateriaPrima>> GetMateriaPrima(int id)
+        {
+            var materiaPrima = await _context.MateriasPrimas.FindAsync(id);
+
+            if (materiaPrima == null)
+            {
+                return NotFound();
+            }
+
+            return materiaPrima;
+        }
+
+        // POST: api/MateriaPrima
+        [HttpPost]
+        public async Task<ActionResult<MateriaPrima>> PostMateriaPrima(MateriaPrima materiaPrima)
+        {
+            _context.MateriasPrimas.Add(materiaPrima);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMateriaPrima", new { id = materiaPrima.Id }, materiaPrima);
+        }
+
+        // Additional methods for PUT, DELETE, etc., can be added here as needed
+    }
+}
