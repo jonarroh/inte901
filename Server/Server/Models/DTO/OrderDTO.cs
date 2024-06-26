7d9a3e75d8a713c87e89163e56f21018221fd3a7
Christoper Ramirez <19002167@alumnos.utleon.edu.mx>
2024-06-26 12:33:51 -0600

    Controllers vacios de compra y orden

--- Server/Server/Models/DTO/OrderDTO.cs
+++ Server/Server/Models/DTO/OrderDTO.cs
@@ -7,6 +7,8 @@
 		public int? IdClient { get; set; }
 		public int? IdUser { get; set; }
 		public float? Total { get; set; }
+		
+		
 		public int? IdDetailOrder { get; set; }
 		public int? IdOrder { get; set; }
 		public int? IdProduct { get; set; }
@@ -16,6 +18,6 @@
 		public int? Status { get; set; }
 		public DateTime? DateOrder { get; set; }
 		public long? Ticket { get; set; }
-		public DateTime? Created_at_detail { get; set; }
+		public DateTime? CreatedAtDetail { get; set; }
 	}
 }
