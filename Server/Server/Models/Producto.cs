b4fcc15755a34fe2f4c1f848dc59f14024ec31f4
jonarroh <jonarrodi99@gmail.com>
2024-06-25 21:56:39 -0600

    chore: added Tipo in Producto Model

--- Server/Server/Models/Producto.cs
+++ Server/Server/Models/Producto.cs
@@ -6,21 +6,30 @@ namespace Server.Models
     {
         [Key]
         public int? Id { get; set; }
+
         [Required]
         [MaxLength(255)]
         public string? Nombre { get; set; }
+
         [Required]
         public decimal? Precio { get; set; }
 
         public string? Descripcion { get; set; }
+
         [Required]
         public int? Estatus { get; set; } = 1;
+
+        [Required]
+        public string Tipo { get; set; }
+
         [Required]
         public int? CantidadXReceta { get; set; }
+
         [Required]
         public DateTime? CreatedAt { get; set; } = DateTime.Now;
 
         public ICollection<Ingrediente>? Ingredientes { get; set; }
+
         public ICollection<InventarioPostre>? InventarioPostres { get; set; }
     }
 }
