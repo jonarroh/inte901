4633b94e501b71099029b8b90da59141957ec8db
jonarroh <jonarrodi99@gmail.com>
2024-06-26 20:43:57 -0600

    chore: added development server

--- Server/Server/Models/Order.cs
+++ Server/Server/Models/Order.cs
@@ -1,23 +1,25 @@
 ﻿namespace Server.Models
 {
-    using Server.Models.Usuario.Server.Models.Usuario;
+   
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Server.Models.Usuario.Server.Models.Usuario;
 
     public class Order
     {
         [Key]
         public int? Id { get; set; }
         public DateTime? OrderDate { get; set; }
-        public int? IdClient { get; set; }
+
+        [ForeignKey("User")]
         public int? IdUser { get; set; }
         [Required]
         public float? Total { get; set; }
         
         
         public ICollection<DetailOrder>? DetailOrders { get; set; }
-        
-        public ICollection<User>? User { get; set; }
+       
     }
 }
