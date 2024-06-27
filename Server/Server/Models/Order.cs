405fa1e3114e61a63f41b6861a4329ab011c54e1
Chistoper Ramirez <85514595+IvanRS9@users.noreply.github.com>
2024-06-26 23:00:08 -0600

    Anotaciones FK -> Orden & Compra

--- Server/Server/Models/Order.cs
+++ Server/Server/Models/Order.cs
@@ -4,13 +4,18 @@
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
 
-    public class Order
+	public class Order
     {
         [Key]
         public int? Id { get; set; }
+        [Required]
         public DateTime? OrderDate { get; set; }
+
         public int? IdClient { get; set; }
+        [ForeignKey("IdUser")]
+        [Required]
         public int? IdUser { get; set; }
         [Required]
         public float? Total { get; set; }
@@ -18,6 +23,6 @@
         
         public ICollection<DetailOrder>? DetailOrders { get; set; }
         
-        public ICollection<User>? User { get; set; }
+        public User? User { get; set; }
     }
 }
