43fde2c8ac18cf051e66df023986f9ac16c0aa24
jonarroh <jonarrodi99@gmail.com>
2024-06-27 14:40:38 -0600

    fix: errors in landing pages

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig,importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import {LucideAngularModule, ArrowUpRight, File, Home, Menu, UserCheck, icons, LoaderCircle} from "lucide-angular";
+import {LucideAngularModule, icons} from "lucide-angular";
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(),
-    importProvidersFrom(LucideAngularModule.pick({LoaderCircle}))],
+    importProvidersFrom(LucideAngularModule.pick(icons))],
 
 };
