346c413caf00a35f52afb259f5dcf4b77f9e3329
UrielP <21000013@alumnos.utleon.edu.mx>
2024-07-09 19:11:24 -0600

    Landing component 2

---
+++ client/src/app/landing-page/section-prod/section-prod.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-section-prod',
+  standalone: true,
+  imports: [LucideAngularModule],
+  templateUrl: './section-prod.component.html',
+  styleUrl: './section-prod.component.css'
+})
+export class SectionProdComponent {
+
+}
