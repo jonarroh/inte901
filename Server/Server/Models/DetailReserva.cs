e76b83ef073097ab087982d27a3967faf81cb167
Jonathan Uriel Rodriguez Hernandez <87589672+jonarroh@users.noreply.github.com>
2024-07-09 21:51:50 -0600

    Update DetailReserva.cs

--- Server/Server/Models/DetailReserva.cs
+++ Server/Server/Models/DetailReserva.cs
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-=======
-﻿using System.ComponentModel.DataAnnotations;
->>>>>>> 88e1a8911d75b8a665bdfee0c40e377ed0b4f7b7
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Server.Models
@@ -12,17 +9,12 @@ namespace Server.Models
     {
         [Key]
         [Required]
-<<<<<<< HEAD
         public int idDetailReser { get; set; }
-=======
-        public int? idDetailReser { get; set; }
->>>>>>> 88e1a8911d75b8a665bdfee0c40e377ed0b4f7b7
 
         [Required]
         public DateTime fecha { get; set; }
 
         [Required]
-<<<<<<< HEAD
         public string horaInicio { get; set; }
 
         [Required]
@@ -33,16 +25,6 @@ namespace Server.Models
         public int idEspacio { get; set; }
 
         public virtual Espacio Espacio { get; set; }
-=======
-        public string? horaInicio { get; set; }
 
-        [Required]
-        public string? horaFin {  get; set; }
-
-        [Required]
-        [ForeignKey("EspacioId")]
-        public int? idEspacio { get; set; }
-
->>>>>>> 88e1a8911d75b8a665bdfee0c40e377ed0b4f7b7
     }
 }
