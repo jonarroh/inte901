2f52fc5531afb0ad40e38337eedaaed21af38ed3
UrielP <21000013@alumnos.utleon.edu.mx>
2024-07-16 11:07:19 -0600

    Landing page v3

--- client/src/app/landing-page/landing-page.component.ts
+++ client/src/app/landing-page/landing-page.component.ts
@@ -4,13 +4,15 @@ import { FooterComponent } from './footer/footer.component';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { SectionProdComponent } from './section-prod/section-prod.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { CalidadComponent } from './calidad/calidad.component';
+import { ValoresComponent } from './valores/valores.component';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, HlmButtonDirective, SectionProdComponent, InicioComponent],
+  imports: [HeaderComponent, FooterComponent, HlmButtonDirective, SectionProdComponent, InicioComponent, CalidadComponent, ValoresComponent],
 })
 export class LandingPageComponent {
 
