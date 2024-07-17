7ac7c2c6d164ade1812b8919599782b38d0da5b8
Christoper Ramirez <19002167@alumnos.utleon.edu.mx>
2024-07-17 13:57:59 -0600

    chore: Update Models

--- Server/Server/Models/DTO/DetailOrderDTO.cs
+++ Server/Server/Models/DTO/DetailOrderDTO.cs
@@ -4,12 +4,11 @@
     {
         public int Id { get; set; }
         public int IdProduct { get; set; }
-        public string NameProduct { get; set; }
+        public int IdOrder { get; set; }
         public int Quantity { get; set; }
         public decimal PriceSingle { get; set; }
-        public int Status { get; set; }
         public DateTime DateOrder { get; set; }
-        public long Ticket { get; set; }
         public string Ingredients { get; set; }
+        public string Status { get; set; }
     }
 }
