71c4a0514d8402f3a6fe61354d27f4575e279f0f
Daniel Valderrama <danimanri05@gmail.com>
2024-07-26 20:14:26 -0600

    Inicié el front de materia_prima

---
+++ client/src/app/admin/ingredientes/ingredientes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredientes',
+  standalone: true,
+  imports: [],
+  templateUrl: './ingredientes.component.html',
+  styleUrl: './ingredientes.component.css'
+})
+export class IngredientesComponent {
+
+}
---
+++ client/src/app/admin/materias-primas/materias-primas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-materias-primas',
+  standalone: true,
+  imports: [],
+  templateUrl: './materias-primas.component.html',
+  styleUrl: './materias-primas.component.css'
+})
+export class MateriasPrimasComponent {
+
+}
