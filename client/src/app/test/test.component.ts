0926addc0b775850d656fa5a94866ad2b0a98380
jonarroh <jonarrodi99@gmail.com>
2024-07-29 14:27:33 -0600

    wip[client]:example of SignalR use

--- client/src/app/test/test.component.ts
+++ client/src/app/test/test.component.ts
@@ -2,17 +2,37 @@ import { Component } from '@angular/core';
 
 
 import { HlmCarouselComponent, HlmCarouselContentComponent,HlmCarouselItemComponent,HlmCarouselNextComponent,HlmCarouselPreviousComponent } from '@spartan-ng/ui-carousel-helm';
+import { SignalRService } from '../orden/signal-rorder.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-test',
   standalone: true,
   imports: [
+    CommonModule,
     HlmCarouselComponent, HlmCarouselContentComponent,HlmCarouselItemComponent,HlmCarouselNextComponent,HlmCarouselPreviousComponent 
   ],
+  providers: [SignalRService],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
 export class TestComponent {
+  updates: string[] = [];
 
+  constructor(private signalRService: SignalRService) {}
+
+  ngOnInit(): void {
+    this.signalRService.hubConnection.on('ReceiveOrderUpdate', (message: string) => {
+      this.updates.push(message);
+      const changeId = message.split(':')[0];
+      const status = message.split(':')[1];
+
+      //en esta partes vas a poder manejar el mensaje recibido
+      //por ejemplo, tienes que ver si el changeId es igual a un id de orden que tengas cargada en tu componente
+      //y si lo es, actualizas el estado de la orden con el status recibido
+      
+
+    });
+  }
 }
