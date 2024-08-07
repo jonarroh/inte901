c831db05849873de4befd142c50373cc05097eb3
UrielP <21000013@alumnos.utleon.edu.mx>
2024-08-06 19:56:50 -0600

    redireccionamiento añadido en lading page

--- client/src/app/landing-page/header/header.component.ts
+++ client/src/app/landing-page/header/header.component.ts
@@ -1,40 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    LucideAngularModule
+    LucideAngularModule,
+    RouterModule
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor() {
+export class HeaderComponent implements AfterViewInit {
+  constructor() { }
 
-        const headerEl = document.querySelector("#landing-header") as HTMLElement
-        console.log(headerEl)
-        console.log("Hola") 
-        
-        const observerOptions = {
-            root: null,
-            rootMargin: '0px', 
-            threshold: 0.6,
-        }
-    
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                const {isIntersecting} = entry 
-                if (isIntersecting) {
-                    const color = entry.target.getAttribute("data-header-color")
-                    console.log(color)
-                    headerEl.style.color = color ?? '';
-                }
-            })
-        }, observerOptions)
-    
-        const sectionsElements = document.querySelectorAll(".landing-section")
-        sectionsElements.forEach((section) => observer.observe(section))
+  ngAfterViewInit() {
+    const headerEl = document.querySelector("#landing-header") as HTMLElement;
+    console.log(headerEl);
+    console.log("Hola");
+
+    if (headerEl) {
+      const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.6,
+      };
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          const { isIntersecting } = entry;
+          if (isIntersecting) {
+            const color = entry.target.getAttribute("data-header-color");
+            console.log(color);
+            headerEl.style.color = color ?? '';
+          }
+        });
+      }, observerOptions);
+
+      const sectionsElements = document.querySelectorAll(".landing-section");
+      sectionsElements.forEach((section) => observer.observe(section));
+    } else {
+      console.error("Header element not found!");
+    }
   }
 }
