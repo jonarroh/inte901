dd171b45f6e5b7de7328646ea017971acaa861e1
Daniel Valderrama <danimanri05@gmail.com>
2024-08-08 23:21:25 -0600

    Modulo producto ya muestra imagenes

---
+++ client/src/app/admin/inventario-mp/inventario-mp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventario-mp',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventario-mp.component.html',
+  styleUrl: './inventario-mp.component.css'
+})
+export class InventarioMPComponent {
+
+}
