3f521d4922d15a59f6abed5984708c08faf9bc76
jonarroh <jonarrodi99@gmail.com>
2024-08-11 18:21:31 -0600

    fix it

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc9eaeb110c9a26cfa4c67641ffbf025e8724cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffee18a9f2d28c481a9b66ff34a3924a51fb7e14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
