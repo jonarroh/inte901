c8b9370fbea3356d1a9fe407d0924b44723df68f
Chistoper Ramirez <85514595+IvanRS9@users.noreply.github.com>
2024-08-13 23:34:39 -0600

    fix: change colors in admin views

--- client/src/components/ui-menu-helm/src/lib/hlm-menu-item-checkbox.directive.ts
+++ client/src/components/ui-menu-helm/src/lib/hlm-menu-item-checkbox.directive.ts
@@ -21,7 +21,7 @@ export class HlmMenuItemCheckboxDirective {
 	public readonly userClass = input<ClassValue>('', { alias: 'class' });
 	protected _computedClass = computed(() =>
 		hlm(
-			'group w-full relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:bg-accent focus-visible:text-accent-foreground disabled:pointer-events-none disabled:opacity-50',
+			'group w-full relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors hover:bg-navhover hover:text-navhover-foreground focus-visible:bg-navhover focus-visible:text-navhover-foreground disabled:pointer-events-none disabled:opacity-50',
 			this.userClass(),
 		),
 	);
