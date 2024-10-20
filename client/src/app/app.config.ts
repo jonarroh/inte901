4e98c5d4e01d0f45ca2ae78a65efc2e913e36040
jonarro0 <jrodriguez@cloudgenia.com>
2024-10-19 22:02:13 -0600

    feat: add pwa config

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,11 @@
 
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { LucideAngularModule, icons } from 'lucide-angular';
 import { routes } from './app.routes';
 import { authInterceptor } from './auth/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 
@@ -12,6 +13,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    importProvidersFrom(LucideAngularModule.pick(icons))
+    importProvidersFrom(LucideAngularModule.pick(icons)), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ],
 };
