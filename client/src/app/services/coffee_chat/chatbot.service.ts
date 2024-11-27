c4db250f32a5354bf1cc65489a302ae4026d5a48
jonarro0 <jrodriguez@cloudgenia.com>
2024-11-26 18:45:26 -0600

    fix brenda errors

--- client/src/app/services/coffee_chat/chatbot.service.ts
+++ client/src/app/services/coffee_chat/chatbot.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ChatbotService {
-  private apiUrl = 'http://localhost:11434/api/chat'; // Chatbot API URL
+  private apiUrl = 'http://192.168.137.196:11434/api/chat'; // Chatbot API URL
 
   constructor(private http: HttpClient) {}
 
