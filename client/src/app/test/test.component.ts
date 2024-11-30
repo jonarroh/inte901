bbf3cc22ce819340e1b0ca028d7f05e127369620
Jonathan Uriel Rodriguez Hernandez <87589672+jonarroh@users.noreply.github.com>
2024-11-30 11:37:29 -0600

    Update test.component.ts

--- client/src/app/test/test.component.ts
+++ client/src/app/test/test.component.ts
@@ -19,7 +19,7 @@ type Framework = { label: string; value: string };
   selector: 'app-test',
   standalone: true,
   imports: [
-    BrnCommandImports,
+    //BrnCommandImports,
     HlmCommandImports,
     HlmIconComponent,
     HlmButtonDirective,
